d61b1a46f764b0386b822cf323543587a75d9862
Fabio Ferrero <fabio.ferrero.93@gmail.com>
2019-03-17 18:23:01 +0100

    [ViewController] Display amounts

--- Amount/ViewController.swift
+++ Amount/ViewController.swift
@@ -20,10 +20,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var currency8: UILabel!
     @IBOutlet weak var currency9: UILabel!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
+        let amount1: Amount = 12000.00
+        currency1.text = String(describing: amount1)
         
+        let amount2: Amount = 12000.00
+        currency2.display(amount: amount2)
+        
+        let amount3: Amount = 12000.00
+        currency3.display(amount: amount3)
+        
+        let amount4: Amount = 12000.00
+        currency4.display(amount: amount4)
+        
+        let amount5: Amount = 12000.00
+        currency5.display(amount: amount5)
+        
+        let amount6: Amount = 12000.00
+        currency6.display(amount: amount6)
+        
+        let amount7: Amount = 12000.00
+        currency7.display(amount: amount7)
+        
+        let amount8: Amount = 12000.00
+        currency8.display(amount: amount8)
+        
+        let amount9: Amount = 12000.00
+        currency9.display(amount: amount9)
     }
 }
 
