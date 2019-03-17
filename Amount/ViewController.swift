7676e74045e8e03c674cc94dbe0f53d1aabf07bc
Fabio Ferrero <fabio.ferrero.93@gmail.com>
2019-03-17 17:13:52 +0100

    [ViewController] Add currency labels

--- Amount/ViewController.swift
+++ Amount/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var currency1: UILabel!
+    @IBOutlet weak var currency2: UILabel!
+    @IBOutlet weak var currency3: UILabel!
+    @IBOutlet weak var currency4: UILabel!
+    @IBOutlet weak var currency5: UILabel!
+    @IBOutlet weak var currency6: UILabel!
+    @IBOutlet weak var currency7: UILabel!
+    @IBOutlet weak var currency8: UILabel!
+    @IBOutlet weak var currency9: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
-
 }
 
